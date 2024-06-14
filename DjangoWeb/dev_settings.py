48f0ea78a11a10266e5bf368c341bdd7d051d3a1
Pavel Lednev <lednevpasha@mail.ru>
2024-06-14 22:45:51 +0600

    responsive design v1.0 (home page)

--- DjangoWeb/dev_settings.py
+++ DjangoWeb/dev_settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = generate_key()
+SECRET_KEY = "urghjrsjgruy745724bc34765rx7643cr23x--234bxr645-c254c2cq45t-c2v4y6c4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
