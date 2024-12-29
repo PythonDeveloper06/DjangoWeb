fe7942fb98e7a3ea3d2baf6909c81bc0aa9a23bd
PythonDeveloper06 <118295084+PythonDeveloper06@users.noreply.github.com>
2024-12-29 22:55:01 +0600

    Update production_settings.py

--- DjangoWeb/production_settings.py
+++ DjangoWeb/production_settings.py
@@ -14,7 +14,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://seld-lock.ru']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
